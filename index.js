2226ee6e062f802716d4ea7612b3c62b38cab82a
Samuel Cornet <samuel@noukod.com>
2019-02-21 20:17:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
 function favoriteIceCream(flavor) {
-  flavor = "mint chocolate chip"
-  debugger;
+  return `I love ${flavor}`;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
 }
